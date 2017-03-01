f8df3aa6a6c3ee13a79006e1521535692dd73d89
Carlos Gonzalez <19charles96@gmail.com>
2017-03-01 15:07:24 +0000

    RESTART

--- site/jstest.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-var jsdom = require("jsdom");
-var html = "<!DOCTYPE html><html><p id='x'>Some text.</html>";
-jsdom.env(html, go);
-
-function go(err, window) {
-    console.log(window.document);
-}
