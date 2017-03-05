32dcd616081a8a3b2204f0efe35ec580ca26b547
Carlos Gonzalez <19charles96@gmail.com>
2017-03-05 14:43:44 +0000

    Basic posts

---
+++ site/public/js/memefy.js
@@ -0,0 +1,42 @@
+"use strict";
+
+function trending(){
+  $("#tabs").find(".sr-only").remove();
+  $("#trendTag").removeClass();
+  $("#newTag").removeClass();
+  $("#topTag").removeClass();
+  $("#memeCreatorTag").removeClass();
+  $("#trendTag").addClass("active");
+  $("#trendingTag").find("a").append('<span class="sr-only">(current)</span>')
+}
+
+function newTag(){
+  $("#tabs").find(".sr-only").remove();
+  $("#trendTag").removeClass();
+  $("#newTag").removeClass();
+  $("#topTag").removeClass();
+  $("#memeCreatorTag").removeClass();
+  $("#newTag").addClass("active");
+  $("#newTag").find("a").append('<span class="sr-only">(current)</span>')
+}
+
+
+function topTag(){
+  $("#tabs").find(".sr-only").remove();
+  $("#trendTag").removeClass();
+  $("#newTag").removeClass();
+  $("#topTag").removeClass();
+  $("#memeCreatorTag").removeClass();
+  $("#topTag").addClass("active");
+  $("#topTag").find("a").append('<span class="sr-only">(current)</span>')
+}
+
+function memeCreator(){
+  $("#tabs").find(".sr-only").remove();
+  $("#trendTag").removeClass();
+  $("#newTag").removeClass();
+  $("#topTag").removeClass();
+  $("#memeCreatorTag").removeClass();
+  $("#memeCreatorTag").addClass("active");
+  $("#memeCreatorTag").find("a").append('<span class="sr-only">(current)</span>')
+}
